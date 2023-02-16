09036d643c8d7ae33dadbe57a1f1030354b83e14
georgynemirovsky <91407260+georgynemirovsky@users.noreply.github.com>
2023-02-16 19:49:57 +0300

    1 - 10 hw2

--- 2/homework.cpp
+++ 2/homework.cpp
@@ -2,12 +2,24 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <cmath>
 
 std::ostream& operator<<(std::ostream& os, std::vector<int> v) {
     for (auto item : v) {
         os << item << ' ';
     }
     return os;
+}
+
+bool is_prime (int value) {
+    double tmp = (double) value;
+    int v = (int) floor(sqrt(tmp));
+    for (int i = 2; i <= v; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 #define range(container) std::begin(container), std::end(container)
@@ -20,17 +32,21 @@ int main() {
     std::cout << "ex1: " << P1 << std::endl;
 
 
+    int n = 0;
+    std::cout << "how many numbers do you want to insert? ";
+    std::cin >> n;
     int a = 0;
-    std::cin >> a;
-    P1.insert(std::end(P1), a);
-    std::cin >> a;
-    P1.insert(std::end(P1), a);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a;
+        P1.push_back(a);
+    }
     std::cout << "ex2: " << P1 << std::endl;
 
 
     std::random_device d;
     std::mt19937_64 mt(d());
     std::uniform_int_distribution<int> distribution(0, P1.size() - 1);
+    std::uniform_int_distribution<int> distribution2(0, 100);
 
     for (int i = 0; i < 100; i++) {
         int random_ind1 = distribution(mt);
@@ -46,6 +62,42 @@ int main() {
     P1.erase(end, std::end(P1));
     std::cout << "ex4: " << P1 << std::endl;
 
+    int num = 0;
+    for (auto it : P1) {
+        if (it % 2 == 1) {
+            num++;
+        }
+    }
+    std::cout << "ex5: " << num << std::endl;
 
+
+    std::cout << "ex6: " << *(std::min_element(range(P1))) << " " << *(std::max_element(range(P1))) << std::endl;
+
+
+    std::vector<int> prime_numbers;
+    for (auto it : P1) {
+        if (is_prime(it)) {
+            prime_numbers.push_back(it);
+        }
+    }
+    std::sort(range(prime_numbers));
+    std::cout << "ex7: " << prime_numbers << std::endl;
+
+
+    std::transform(std::begin(P1), std::end(P1), std::begin(P1), [](auto item) {
+                                                                                return item * item;
+                                                                                });
+    std::cout << "ex8: " << P1 << std::endl;
+
+
+    std::vector<int> P2;
+    for (int i = 0; i < P1.size(); i++) {
+        auto random_number = distribution2(mt);
+        P2.push_back(random_number);
+    }
+    std::cout << "ex9: " << P2 << std::endl;
+
+
+    std::cout << "ex10: " << std::accumulate(range(P2), 0);
     return 0;
 }
